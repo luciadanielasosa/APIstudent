a0f4b44f0b29d12452c6071b9087bd97467e6817
luciadanielasosa <a2019118793@estudiantes.upsa.edu.bo>
2020-08-22 12:09:36 -0400

    Con StudentController

---
+++ APIstudent/APIstudent1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="APIstudent1.WebApiApplication" Language="C#" %>
---
+++ APIstudent/APIstudent1/Models/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace APIstudent1.Models
+{
+    public class DataContext: DbContext 
+    {
+        public DataContext(): base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<APIstudent1.Models.Student> Students { get; set; }
+    }
+}
